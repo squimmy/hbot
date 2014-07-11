f07125f5469d2536c362c18de4c5ba44713e6cf0
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-07-12 00:18:05 +0800

    simple interface for parsing/evaluating dice

---
+++ Dice.hs
@@ -0,0 +1,19 @@
+module Dice (
+  evaluate
+) where
+
+
+import Dice.AST
+import Dice.Parser
+import Dice.Evaluator
+import System.Random
+
+evaluate :: String -> Maybe (IO ([Int], Int))
+evaluate input = case parseDice input of
+        Left _ -> Nothing
+        Right tree -> Just (do
+            g <- getStdGen
+            return $ evalTree (roll g) tree)
+
+roll :: RandomGen a => a -> Int -> Int -> [Int]
+roll g c d = take c (randomRs (1, d) g)
