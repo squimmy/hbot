e757ba7e8deb109fae27167e1033f176dac15eb1
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-07-15 04:17:57 +0800

    font/colour formatting for irc messages

---
+++ Irc/Formatting.hs
@@ -0,0 +1,65 @@
+module Irc.Formatting
+( Colour(..)
+, Font(..)
+, format
+) where
+
+class Encode a where
+    encode :: a -> String
+
+data Colour = White
+            | Black
+            | Blue
+            | Green
+            | Red
+            | Brown
+            | Purple
+            | Orange
+            | Yellow
+            | Lime
+            | Teal
+            | Cyan
+            | LightBlue
+            | Pink
+            | Grey
+            | Silver deriving Show
+
+instance Encode Colour where
+    encode c = "\ETX" ++ (getCode c)
+
+instance Encode (Colour, Colour) where
+    encode (c, c') = "\ETX" ++ (getCode c) ++ "," ++ (getCode c')
+
+getCode :: Colour -> String
+getCode c = case c of
+    White -> "00"
+    Black -> "01"
+    Blue -> "02"
+    Green -> "03"
+    Red -> "04"
+    Brown -> "05"
+    Purple -> "06"
+    Orange -> "07"
+    Yellow -> "08"
+    Lime -> "09"
+    Teal -> "10"
+    Cyan -> "11"
+    LightBlue -> "12"
+    Pink -> "13"
+    Grey -> "14"
+    Silver -> "15"
+
+data Font = Bold
+          | Italic
+          | Underline deriving Show
+
+instance Encode Font where
+    encode f = case f of
+        Bold -> "\STX"
+        Italic -> "\SYN"
+        Underline -> "\US"
+
+
+format :: Encode a => a -> String -> String
+format e x = (encode e) ++ x ++ "\SI"
+
