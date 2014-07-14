ef1d0e0e34381ebbb513229a237df3e88afe7ca6
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-07-14 22:20:55 +0800

    include more information with rolled dice

--- Dice.hs
+++ Dice.hs
@@ -8,12 +8,12 @@ import Dice.Parser
 import Dice.Evaluator
 import System.Random
 
-evaluate :: String -> Maybe (IO ([Int], Int))
+evaluate :: String -> Maybe (IO ([Die], Int))
 evaluate input = case parseDice input of
         Left _ -> Nothing
         Right tree -> Just (do
             g <- newStdGen
             return $ evalTree roll g tree)
 
-roll :: RandomGen a => a -> Int -> Int -> [Int]
-roll g c d = take c (randomRs (1, d) g)
+roll :: RandomGen a => a -> Int -> Int -> [Die]
+roll g c d = zipWith Die (repeat d) (take c (randomRs (1, d) g))
--- Dice/Evaluator.hs
+++ Dice/Evaluator.hs
@@ -1,38 +1,45 @@
-module Dice.Evaluator (
- evalTree
+module Dice.Evaluator
+( evalTree
+, Die(Die)
 ) where
 
 import Data.List
 import Dice.AST
 import System.Random
 
-type RollDice = RandomGen a => a -> Int -> Int -> [Int]
+data Die = Die { sides :: Int
+               , value :: Int
+               } deriving Show
 
-evalTree :: RandomGen a => RollDice -> a -> Sum -> ([Int], Int)
+type RollDice = RandomGen a => a -> Int -> Int -> [Die]
+
+evalTree :: RandomGen a => RollDice -> a -> Sum -> ([Die], Int)
 evalTree = evalSum
 
-evalRoll :: RandomGen a => RollDice -> a -> Roll -> ([Int], Int)
+evalRoll :: RandomGen a => RollDice -> a -> Roll -> ([Die], Int)
 evalRoll r g (Roll x y) = case y of
-    Just d -> (z, sum z) where z = r g x d
+    Just d -> (z, foldl add 0 z)
+        where z = r g x d
+              add n (Die _ v) = n + v
     Nothing -> ([], x)
 
-evalExpr :: RandomGen a => RollDice -> a -> Expr -> ([Int], Int)
+evalExpr :: RandomGen a => RollDice -> a -> Expr -> ([Die], Int)
 evalExpr r g e = case e of
     Expr x -> evalRoll r g x
     Parens x -> evalSum r g x
 
-evalProd :: RandomGen a => RollDice -> a -> Product -> ([Int], Int)
+evalProd :: RandomGen a => RollDice -> a -> Product -> ([Die], Int)
 evalProd r g (Product x xs) = (foldr1 (&*) (map (uncurry (evalExpr r)) (zip (splitMany g) (x:xs))))
     where (ys, y) &* (zs, z) = (ys++zs, y*z)
 
-evalSum :: RandomGen a => RollDice -> a -> Sum -> ([Int], Int)
+evalSum :: RandomGen a => RollDice -> a -> Sum -> ([Die], Int)
 evalSum r g (Sum x xs) = foldl apply (evalProd r g1 x) (zipWith (evalOp r) (splitMany g2) xs)
     where apply (ys, y) (op, (zs, z)) = case op of
               Plus -> (ys++zs, y+z)
               Minus -> (ys++zs, y-z)
           (g1, g2) = split g
 
-evalOp :: RandomGen a => RollDice -> a -> (SumType, Product) -> (SumType, ([Int], Int))
+evalOp :: RandomGen a => RollDice -> a -> (SumType, Product) -> (SumType, ([Die], Int))
 evalOp r g (op, prod) = (op, evalProd r g prod)
 
 splitMany :: RandomGen a => a -> [a]
